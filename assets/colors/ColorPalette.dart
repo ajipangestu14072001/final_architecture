e777eac431d70a3678c96f54ed710d5baaccc750
Afif <afifandika@gmail.com>
2024-09-10 17:13:05 +0700

    TODO: Add color palette

---
+++ assets/colors/ColorPalette.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  // TODO: Confirm color text default
+
+  /// ==================================== brand color ==================================== ///
+  //brand color Group
+  // Primary
+  static const Color primaryBrandColor50 = Color(0xFFF9F5FF);
+  static const Color primaryBrandColor100 = Color(0xFFE6D8FE);
+  static const Color primaryBrandColor200 = Color(0xFFD6BFFE);
+  static const Color primaryBrandColor300 = Color(0xFFC19EFE);
+  static const Color primaryBrandColor400 = Color(0xFFAC7DFD);
+  static const Color primaryBrandColor500 = Color(0xFF833CFC);
+  static const Color primaryBrandColor600 = Color(0xFF6D32D2);
+  static const Color primaryBrandColor700 = Color(0xFF5728A8);
+  static const Color primaryBrandColor800 = Color(0xFF4421FE);
+  static const Color primaryBrandColor900 = Color(0xFF421E7E);
+  static const Color primaryBrandColor950 = Color(0xFF2C1454);
+  static const Color primaryBrandColor1000 = Color(0xFF1A0C32);
+  // Secondary
+  static const Color secondaryBrandColor50 = Color(0xFFFCFFF5);
+  static const Color secondaryBrandColor100 = Color(0xFFF5FFD5);
+  static const Color secondaryBrandColor200 = Color(0xFFEEFFB9);
+  static const Color secondaryBrandColor300 = Color(0xFFE5FF97);
+  static const Color secondaryBrandColor400 = Color(0xFFD6FF74);
+  static const Color secondaryBrandColor500 = Color(0xFFD4FF51);
+  static const Color secondaryBrandColor600 = Color(0xFFCBFF2E);
+  static const Color secondaryBrandColor700 = Color(0xFFA9D526);
+  static const Color secondaryBrandColor800 = Color(0xFF87AA1F);
+  static const Color secondaryBrandColor900 = Color(0xFF668017);
+  static const Color secondaryBrandColor950 = Color(0xFF44550F);
+  static const Color secondaryBrandColor1000 = Color(0xFF293309);
+  // Tersier
+  static const Color tersierBrandColor50 = Color(0xFFFCFCFC);
+  static const Color tersierBrandColor100 = Color(0xFFF7F7FA);
+  static const Color tersierBrandColor200 = Color(0xFFF1F1F6);
+  static const Color tersierBrandColor300 = Color(0xFFEDEDF2);
+  static const Color tersierBrandColor400 = Color(0xFFE5E5E9);
+  static const Color tersierBrandColor500 = Color(0xFFD1D1D5);
+  static const Color tersierBrandColor600 = Color(0xFFB2B3B8);
+  static const Color tersierBrandColor700 = Color(0xFF8B8D95);
+  static const Color tersierBrandColor800 = Color(0xFF667772);
+  static const Color tersierBrandColor900 = Color(0xFF3F414E);
+  static const Color tersierBrandColor950 = Color(0xFF26293B);
+  static const Color tersierBrandColor1000 = Color(0xFF181B2B);
+  // Neutral White
+  static const Color neutralWhite = Color(0xFFFFFFFF);
+}
